bbcedd0e201ed2012dfb9d0842f316bda398ea0a
dclown13 <w6092w@gmail.com>
2023-09-08 17:20:11 +0900

    09-08

---
+++ src/main/java/com/kh/auction/controller/CommentsController.java
@@ -0,0 +1,45 @@
+package com.kh.auction.controller;
+
+import com.kh.auction.domain.Comments;
+import com.kh.auction.service.CommentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/*")
+public class CommentsController {
+
+    @Autowired
+    private CommentsService service;
+
+    @GetMapping("/comments")
+    public ResponseEntity<List<Comments>> showAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.showAll());
+    }
+
+
+    @GetMapping("/comments/{id}")
+    public ResponseEntity<Comments> show(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.show(id));
+    }
+
+    @PostMapping("/comments")
+    public ResponseEntity<Comments> create(@PathVariable Comments comments) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.create(comments));
+    }
+
+    @PutMapping("/comments")
+    public ResponseEntity<Comments> update(@PathVariable Comments comments) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.update(comments));
+    }
+
+    @DeleteMapping("/comments/{id}")
+    public ResponseEntity<Comments> delete(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
+    }
+
+}
