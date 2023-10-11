2a77599d1c355041aed9c197b0198fc34552e854
wonku19 <133620204+wonku19@users.noreply.github.com>
2023-10-11 17:24:54 +0900

    1011
    덮어씌움

--- src/main/java/com/kh/auction/controller/AuctionBoardController.java
+++ src/main/java/com/kh/auction/controller/AuctionBoardController.java
@@ -5,7 +5,6 @@ import com.kh.auction.domain.Category;
 import com.kh.auction.domain.Comments;
 import com.kh.auction.service.AuctionBoardService;
 import com.kh.auction.service.CategoryService;
-import com.kh.auction.service.CommentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +20,7 @@ public class AuctionBoardController {
     private AuctionBoardService service;
 
     @Autowired
-    private CommentsService commentsService;
-
-    @Autowired
     private CategoryService category;
-
     @GetMapping("/auction")
     public ResponseEntity<List<AuctionBoard>> showAll() {
         return ResponseEntity.status(HttpStatus.OK).body(service.showAll());
@@ -55,29 +50,12 @@ public class AuctionBoardController {
         return ResponseEntity.status(HttpStatus.OK).body(service.delete(no));
     }
     // 카테고리
-    @GetMapping("/auction/{auctionNo}/category")
+    @GetMapping("/auction/{auctionNo}")
     public ResponseEntity<List<Category>> categoryList(@PathVariable int auctionNo){
         return ResponseEntity.status(HttpStatus.OK).body(category.findByAuctionNo(auctionNo));
     }
 
-    // 특정 경매 게시글에 달린 댓글들 조회
-    @GetMapping("/auction/{no}/comments")
-    public ResponseEntity<List<Comments>> showComments(@PathVariable int no) {
-        return ResponseEntity.status(HttpStatus.OK).body(commentsService.boardCommentsList(no));
-    }
-    // 경매 번호로 아이템 상세정보 조회
-    @GetMapping("/auction/itemDetails/{itemNo}")
-        public ResponseEntity<AuctionItemDetails> getItemDetailsByItemNo(@PathVariable int itemNo) {
-            AuctionItemDetails itemDetails = auctionItemDetailsService.getAuctionItemDetailsByItemNo(itemNo);
-            try{ 
-                 return ResponseEntity.status(HttpStatus.OK).body(itemDetails);
-            } ecatch (Exception e){
-                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-            }
-    // 경매 번호로 배송 정보 조회
-    @GetMapping("/auction/{auctionNo}")
-    public ResponseEntity<List<Delivery>> getDeliveryByAuctionNo(@PathVariable int auctionNo) {
-        List<Delivery> deliveries = deliveryService.getDeliveryByAuctionNo(auctionNo);
-        return ResponseEntity.status(HttpStatus.OK).body(deliveries);
-    }
+
+
+
 }
